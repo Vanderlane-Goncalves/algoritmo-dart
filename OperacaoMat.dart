6672e32a361c292399d097f5b29a72295160654c
Vanderlane-Goncalves <vanderlanegoncalvesjr@gmail.com>
2021-09-22 15:55:19 -0300

    Exemplos de operações matemáticas
    Código em dart, para operações matemáticas

---
+++ OperacaoMat.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+
+main() {
+  var num1 = 10;
+  var num2 = 2;
+
+  var soma = num1 + num2;
+  var subtracao = num1 - num2;
+  var divisao = num1 / num2;
+  var mod = num1 % num2;
+  var expressao = 2 + 2 * 8 / num2;
+  
+  print(expressao);
+}
