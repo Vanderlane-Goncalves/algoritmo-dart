1365f8b1c3ecd69f07cafd0f6a0028f187ca9b56
Vanderlane-Goncalves <vanderlanegoncalvesjr@gmail.com>
2021-09-22 17:08:45 -0300

    Estrutura básica do algoritmo com a descrição em português
    Nessa primeira etapa, fiz uma pequena descrição do algoritmo e a estrutura correspondente em dart

---
+++ primeiro-algoritmo.dart
@@ -0,0 +1,15 @@
+main(List<String> args) {
+  // *ALGORITMO*
+  // pergunta a idade da pessoa
+  //se a idade for maior de 18
+  //  Ele é maior de idade;
+  //se não for
+  //  Ele não é maior de idade
+
+  var idade;
+  if (idade >= 18) {
+    print("maior de idade");
+  } else {
+    print("menos de idade");
+  }
+}
