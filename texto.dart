b8cdc812ad1643e88b1589893aee225885d9027e
Vanderlane-Goncalves <vanderlanegoncalvesjr@gmail.com>
2021-09-22 16:04:45 -0300

    Concatenação de strings dart
    Concatenação com o sinal + para unir os textos, podendo ele ser em variáveis

---
+++ texto.dart
@@ -0,0 +1,8 @@
+main(List<String> args) {
+  var texto1 = "Vanderlane ";
+  var texto2 = "Gonçalves";
+  var texto = texto1 + texto2;//concatenação
+  
+  //var texto = "Hello " + "World";
+  print(texto);
+}
