edbe725505003c085a0cfa09b9abe50cff353bbf
Vanderlane-Goncalves <vanderlanegoncalvesjr@gmail.com>
2021-09-22 16:53:31 -0300

    Amostragem de valores booleanos Estrutura de de dados com operadores
    Comentários de operadores  e um exemplo de igualdade.

--- EstruturaDeDecisao.dart
+++ EstruturaDeDecisao.dart
@@ -1,6 +1,7 @@
 main(List<String> args) {
-
-  var isTrue = true; //Variavel booleana
+  //var isTrue = 1 > 0; //Variavel booleana
+  // Operadores: + soma, > maior, menor <, ==igualdade, != diferente retorna valores boleanos
+  var isTrue = 1 == 0;
 
   if (isTrue) {
     print("Isso é verdadeiro");
