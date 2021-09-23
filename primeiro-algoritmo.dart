e0ce1b79f31023756a130ceb69f31154e2f1b97b
Vanderlane-Goncalves <vanderlanegoncalvesjr@gmail.com>
2021-09-23 00:19:56 -0300

    Tratamento com comentários
    Nessa parte deixei bem explicado o algoritmo e as minha dificuldades encontradas.

--- primeiro-algoritmo.dart
+++ primeiro-algoritmo.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-main(List<String> args) {
+void main() {
   // *ALGORITMO*
   // pergunta a idade da pessoa
   //se a idade for maior de 18
@@ -8,12 +8,18 @@ main(List<String> args) {
   //se não for
   //  Ele não é maior de idade
 
-  var input = stdin.readLineSync(); // leia uma linha externa sincronamente
-  var idade = int.tryParse(input!);
-
-  if (idade! >= 18) {
-    print("maior de idade");
-  } else {
-    print("menos de idade");
+  var input = stdin.readLineSync(); // Essa linha adquire a variavel
+  //tentei usar o var idade = int.perse(input);
+  //porém sempre dava erro por conta dos numeros nulos, nesse caso tive que
+  //fazer a restruturação do code.
+  if (input != null) {// essa linha testa se o valor é diferente de nulo
+    int? idade = int.tryParse(input);//aqui surge a variavel int(numero inteiro) e o tryParse "pega"
+    if (idade != null) {// essa linha testa se o valor é diferente de nulo
+      if (idade >= 18) {
+        print("maior de idade");
+      } else {
+        print("menos de idade");
+      }
+    }
   }
 }
